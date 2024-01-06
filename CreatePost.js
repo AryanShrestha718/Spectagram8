545312bbae1dd0b04582c45ec77d881c2a7a4b5f
AryanShrestha718 <125922412+AryanShrestha718@users.noreply.github.com>
2024-01-05 20:42:17 -0500

    Add files via upload

---
+++ CreatePost.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+
+export default class CreatePost extends Component {
+    render() {
+        return (
+            <View>
+                if() {
+
+                } else {
+
+                }
+            </View>
+        )
+    }
+}
